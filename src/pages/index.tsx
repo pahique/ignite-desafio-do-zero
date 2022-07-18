d24992ccdf539d966178c0112969f266b00abfe1
Paulo Oliveros <V8T2Z6L@johndeere.com>
2022-07-17 22:35:10 -0300

    Home working without style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      <main className="container">
+        <header className="headerTitle">
+          <img src="/images/logo_full.svg" alt="spacetraveling" />
+        </header>
+        <section className="posts">
+          {postsPagination.results.map(post => (
+            <article key={post.uid}>
+              <Link href={`/post/${post.uid}`}>{post.data.title}</Link>
+              <span className="subtitle">{post.data.subtitle}</span>
+              <div className="publicationDate">
+                {post.first_publication_date}
+              </div>
+              <div className="author">{post.data.author}</div>
+            </article>
+          ))}
+          {postsPagination.next_page && (
+            <button type="button">Carregar mais posts</button>
+          )}
+        </section>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts');
+
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    } as Post;
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      } as PostPagination,
+    },
+  };
+};
